5a9974a72296f7bbca10a179cdee85845de99aef
ming <dxuanming@sjtu.edu.cn>
2025-03-12 15:17:03 +0800

    第5个点RE，析构存在leak

--- priority_queue/data/one/code.cpp
+++ priority_queue/data/one/code.cpp
@@ -1,80 +1,80 @@
+#include <cassert>
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 #include <queue>
-#include <ctime>
-#include <cstdlib>
 
 #include "priority_queue.hpp"
 
-void TestConstructorAndPush()
-{
-	std::cout << "Testing constructors, destructor and push..." << std::endl;
-	sjtu::priority_queue<int> pq;
-	for (int i = 100; i > 0; --i) {
-		pq.push(i);
-	}
-	while (!pq.empty()) {
-		std::cout << pq.top() << " ";
-		pq.pop();
-	}
-	std::cout << std::endl;
-	for (int i = 1000; i > 100; --i) {
-		pq.push(i);
-	}
-	sjtu::priority_queue<int> pqBack(pq);
-	std::cout << pqBack.size() << std::endl;
-	sjtu::priority_queue<int> pqBB;
-	for (int i = 10; i <= 10000; ++i) {
-		pqBB.push(i);
-	}
-	std::cout << pqBB.size() << std::endl;
-	pqBB = pq;
-	std::cout << pqBB.size() << std::endl;
+void TestConstructorAndPush() {
+    std::cout << "Testing constructors, destructor and push..." << std::endl;
+    sjtu::priority_queue<int> pq;
+    for (int i = 100; i > 0; --i) {
+        pq.push(i);
+    }
+    while (!pq.empty()) {
+        std::cout << pq.top() << " ";
+        pq.pop();
+    }
+    std::cout << std::endl;
+    for (int i = 1000; i > 100; --i) {
+        pq.push(i);
+    }
+    sjtu::priority_queue<int> pqBack(pq);
+    std::cout << pqBack.size() << std::endl;
+    sjtu::priority_queue<int> pqBB;
+    for (int i = 10; i <= 10000; ++i) {
+        pqBB.push(i);
+    }
+    std::cout << pqBB.size() << std::endl;
+    pqBB = pq;
+    std::cout << pqBB.size() << std::endl;
 }
 
-void TestSize()
-{
-	std::cout << "Testing size()" << std::endl;
-	sjtu::priority_queue<long long> pq;
-	for (int i = 1; i <= 1000; ++i) {
-		pq.push(rand());
-	}
-	std::cout << pq.size() << std::endl;
+void TestSize() {
+    std::cout << "Testing size()" << std::endl;
+    sjtu::priority_queue<long long> pq;
+    for (int i = 1; i <= 1000; ++i) {
+        pq.push(rand());
+    }
+    std::cout << pq.size() << std::endl;
 }
 
-void TestException()
-{
-	sjtu::priority_queue<int> pq;
-	try {
-		std::cout << pq.top() << std::endl;
-	} catch (...) {
-		std::cout << "Throw correctly." << std::endl;
-	}
+void TestException() {
+    sjtu::priority_queue<int> pq;
+    try {
+        std::cout << pq.top() << std::endl;
+    } catch (...) {
+        std::cout << "Throw correctly." << std::endl;
+    }
 }
 
 struct Natural {
     int x;
 
-    Natural(int _x = 0) { x = _x; }
+    Natural(int _x = 0) {
+        x = _x;
+    }
 
     friend bool operator<(const Natural &lhs, const Natural &rhs) {
-        if (lhs.x < 0 || rhs.x < 0)
-            throw sjtu::runtime_error();
+        if (lhs.x < 0 || rhs.x < 0) throw sjtu::runtime_error();
         return lhs.x < rhs.x;
     }
 };
 
 void TestCompareException() {
-	std::cout << "Testing compare exception...";
+    std::cout << "Testing compare exception...";
 
-	sjtu::priority_queue<Natural> pq;
+    sjtu::priority_queue<Natural> pq;
     static int dat[2000], ans[2000];
-	int pos = 0;
+    int pos = 0;
     for (int i = 1; i <= 1000; ++i) {
         dat[i] = i;
-		if (rand() % 10 == 0) 
-			dat[i] = -i;
-		else ans[++pos] = i;
-	}
+        if (rand() % 10 == 0)
+            dat[i] = -i;
+        else
+            ans[++pos] = i;
+    }
 
     for (int i = 1000; i > 1; --i) {
         unsigned int x = rand();
@@ -88,32 +88,27 @@ void TestCompareException() {
         std::swap(dat[1], dat[p]);
     }
 
-
     for (int i = 1; i <= 1000; ++i) {
         try {
             pq.push(Natural(dat[i]));
         } catch (sjtu::runtime_error) {
-            if (dat[i] >= 0)
-                return std::cout << std::endl, void();
+            if (dat[i] >= 0) return std::cout << std::endl, void();
         }
     }
 
     for (int i = pos; i; --i) {
         Natural t = pq.top();
-        if (t.x != ans[i])
-                return std::cout << std::endl, void();
+        if (t.x != ans[i]) return std::cout << std::endl, void();
         pq.pop();
     }
 
     std::cout << "ok." << std::endl;
-
 }
 
-int main(int argc, char *const argv[])
-{
-	TestConstructorAndPush();
-	TestSize();
-	TestException();
+int main(int argc, char *const argv[]) {
+    TestConstructorAndPush();
+    TestSize();
+    TestException();
     TestCompareException();
-	return 0;
+    return 0;
 }
--- priority_queue/src/priority_queue.hpp
+++ priority_queue/src/priority_queue.hpp
@@ -3,80 +3,161 @@
 
 #include <cstddef>
 #include <functional>
+
 #include "exceptions.hpp"
 
 namespace sjtu {
 /**
  * @brief a container like std::priority_queue which is a heap internal.
- * **Exception Safety**: The `Compare` operation might throw exceptions for certain data.
- * In such cases, any ongoing operation should be terminated, and the priority queue should be restored to its original state before the operation began.
+ * **Exception Safety**: The `Compare` operation might throw exceptions for
+ * certain data. In such cases, any ongoing operation should be terminated, and
+ * the priority queue should be restored to its original state before the
+ * operation began.
  */
-template<typename T, class Compare = std::less<T>>
+template <typename T, class Compare = std::less<T> >
 class priority_queue {
-public:
-	/**
-	 * @brief default constructor
-	 */
-	priority_queue() {}
+   public:
+    struct Node {
+        T value;
+        Node *child = nullptr, *sibling = nullptr;
+        Node(T val) : value(val) {
+        }
+    };
 
-	/**
-	 * @brief copy constructor
-	 * @param other the priority_queue to be copied
-	 */
-	priority_queue(const priority_queue &other) {}
+    /**
+     * @brief default constructor
+     */
+    priority_queue() {
+        queue_size = 0;
+        root = nullptr;
+    };
 
-	/**
-	 * @brief deconstructor
-	 */
-	~priority_queue() {}
+    /**
+     * @brief copy constructor
+     * @param other the priority_queue to be copied
+     */
 
-	/**
-	 * @brief Assignment operator
-	 * @param other the priority_queue to be assigned from
-	 * @return a reference to this priority_queue after assignment
-	 */
-	priority_queue &operator=(const priority_queue &other) {}
+    void HeapCopy(Node *&a, Node *b) {
+        if (b == nullptr) return;
+        a = new Node(b->value);
+        HeapCopy(a->child, b->child);
+        HeapCopy(a->sibling, b->sibling);
+    }
 
-	/**
-	 * @brief get the top element of the priority queue.
-	 * @return a reference of the top element.
-	 * @throws container_is_empty if empty() returns true
-	 */
-	const T & top() const {}
+    priority_queue(const priority_queue &other) {
+        queue_size = other.queue_size;
+        HeapCopy(root, other.root);
+    }
 
-	/**
-	 * @brief push new element to the priority queue.
-	 * @param e the element to be pushed
-	 */
-	void push(const T &e) {}
+    /**
+     * @brief deconstructor
+     */
 
-	/**
-	 * @brief delete the top element from the priority queue.
-	 * @throws container_is_empty if empty() returns true
-	 */
-	void pop() {}
+    void DeleteHeap(Node *&a) {
+        if (a == nullptr) return;
+        DeleteHeap(a->child);
+        DeleteHeap(a->sibling);
+        delete a;
+        a = nullptr;
+    }
 
-	/**
-	 * @brief return the number of elements in the priority queue.
-	 * @return the number of elements.
-	 */
-	size_t size() const {}
+    ~priority_queue() {
+        DeleteHeap(root);
+    }
 
-	/**
-	 * @brief check if the container is empty.
-	 * @return true if it is empty, false otherwise.
-	 */
-	bool empty() const {}
+    /**
+     * @brief Assignment operator
+     * @param other the priority_queue to be assigned from
+     * @return a reference to this priority_queue after assignment
+     */
+    priority_queue &operator=(const priority_queue &other) {
+        if (this == &other) {
+            return *this;
+        }
+        queue_size = other.queue_size;
+        HeapCopy(root, other.root);
+        return *this;
+    }
 
-	/**
-	 * @brief merge another priority_queue into this one.
-	 * The other priority_queue will be cleared after merging.
-	 * The complexity is at most O(logn).
-	 * @param other the priority_queue to be merged.
-	 */
-	void merge(priority_queue &other) {}
+    /**
+     * @brief get the top element of the priority queue.
+     * @return a reference of the top element.
+     * @throws container_is_empty if empty() returns true
+     */
+    const T &top() const {
+        if (empty()) throw container_is_empty();
+        return root->value;
+    }
+
+    Node *Meld(Node *a, Node *b) {
+        if (a == nullptr) return b;
+        if (b == nullptr) return a;
+        if (Compare{}(a->value, b->value)) std::swap(a, b);
+        b->sibling = a->child;
+        a->child = b;
+        return a;
+    }
+
+    /**
+     * @brief push new element to the priority queue.
+     * @param e the element to be pushed
+     */
+    void push(const T &e) {
+        queue_size++;
+        Node *new_node = new Node(e);
+        root = Meld(root, new_node);
+    }
+
+    Node *Merges(Node *x) {
+        if (x == nullptr || x->sibling == nullptr) return x;
+        Node *y = x->sibling;
+        Node *z = y->sibling;
+        x->sibling = y->sibling = nullptr;
+        return Meld(Merges(z), Meld(x, y));
+    }
+
+    /**
+     * @brief delete the top element from the priority queue.
+     * @throws container_is_empty if empty() returns true
+     */
+    void pop() {
+        if (empty()) throw container_is_empty();
+        queue_size--;
+        Node *temp = Merges(root->child);
+        root = temp;
+    }
+
+    /**
+     * @brief return the number of elements in the priority queue.
+     * @return the number of elements.
+     */
+    size_t size() const {
+        return queue_size;
+    }
+
+    /**
+     * @brief check if the container is empty.
+     * @return true if it is empty, false otherwise.
+     */
+    bool empty() const {
+        return queue_size == 0;
+    }
+
+    /**
+     * @brief merge another priority_queue into this one.
+     * The other priority_queue will be cleared after merging.
+     * The complexity is at most O(logn).
+     * @param other the priority_queue to be merged.
+     */
+    void merge(priority_queue &other) {
+        queue_size += other.queue_size;
+        Meld(root, other.root);
+    }
+
+   public:
+    Node *root = nullptr;
+    size_t queue_size = 0;
 };
-
-}
+}  // namespace sjtu
 
 #endif
